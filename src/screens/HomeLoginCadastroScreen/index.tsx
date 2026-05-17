3c41852607a2538273ce31c8751d6c289160c686
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-17 14:42:46 -0300

    feat(HomeLoginCadastroScreen): criado titulo e subtitulo

--- src/screens/HomeLoginCadastroScreen/index.tsx
+++ src/screens/HomeLoginCadastroScreen/index.tsx
@@ -1,9 +1,36 @@
 import { Text, View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+import { COLORS, FONTS } from "../../styles/styles";
 
 export default function HomeLoginCadastroScreen() {
   return (
-    <View>
-      <Text>HomeLoginCadastro</Text>
-    </View>
+    <LayoutWrapper paddingHorizontal={30}>
+        <View 
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+          }}
+        >
+            <Text
+              style={{
+                color: COLORS.orange[900],
+                fontFamily: FONTS.poppins[700],
+                fontSize: 32,
+              }}
+            >
+              Boas vindas ao {"\n"}
+              Pet Guardian!
+            </Text>
+            <Text
+              style={{
+                color: COLORS.orange[700],
+                fontFamily: FONTS.poppins[700],
+                fontSize: 20,
+              }}
+            >
+              Porque cada pet merece cuidado, proteção e carinho todos os dias.
+            </Text>
+        </View>
+    </LayoutWrapper>
   );
 }
