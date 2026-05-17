b704a87b5292b735a03be7fb62a67c6494afdf18
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-17 14:28:45 -0300

    style(App): feito a importação do Poppins

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./src/navigation/AppNavigator";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": Poppins_400Regular,
+    "Poppins-Medium": Poppins_500Medium,
+    "Poppins-SemiBold": Poppins_600SemiBold,
+    "Poppins-Bold": Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <AppNavigator />
