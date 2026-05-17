cf06422e5f8da68d79bb5bc2fa545c6e089d23c7
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-17 12:08:47 -0300

    feat(App): criado NavigationContainer e importado AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
