7b1453dbb87e42cd40a8deb3d6d7b9ad71f46e8d
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-18 10:55:47 -0300

    refactor(ResponsibleRegisterScreen): alterado RegisterScreen para ResponsibleRegisterScreen

---
+++ src/screens/ResponsibleRegisterScreen/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+import ButtonBack from "../../components/ButtonBack";
+import { COLORS, FONTS } from "../../styles/styles";
+import FormResponsible from "../../components/FormResponsible";
+
+export default function ResponsibleRegisterScreen() {
+  return (
+    <LayoutWrapper>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <ButtonBack sizeIcon={35} colorIcon="black" textTrue={true} />
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: FONTS.poppins[400],
+            color: COLORS.orange[900]
+          }}
+        >
+          Passo 1 de 3
+        </Text>
+      </View>
+      <FormResponsible />
+    </LayoutWrapper>
+  );
+}
