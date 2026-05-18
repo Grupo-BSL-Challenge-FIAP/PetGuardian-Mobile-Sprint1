688cd40a02f46d35175ba648ed0be61b4bda529a
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-17 22:42:03 -0300

    feat(TitleOrange): adicionado textAlign como prop

--- src/components/TitleOrange/index.tsx
+++ src/components/TitleOrange/index.tsx
@@ -2,13 +2,14 @@ import { Text } from "react-native";
 import { COLORS, FONTS } from "../../styles/styles";
 import { TitleOrangeType } from "../../types/TitleOrangeType";
 
-export default function TitleOrange({ title, fontSize }: TitleOrangeType) {
+export default function TitleOrange({ title, fontSize, textAlign }: TitleOrangeType) {
   return (
     <Text
       style={{
         color: COLORS.orange[900],
         fontFamily: FONTS.poppins[700],
         fontSize: fontSize || 32,
+        textAlign: textAlign || "left",
       }}
     >
       {title}
