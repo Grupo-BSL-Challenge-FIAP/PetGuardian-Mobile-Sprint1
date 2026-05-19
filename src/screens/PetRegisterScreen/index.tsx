5b35482e25cb362d03e6c9c6c038ed02b2a5a2de
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 03:33:50 -0300

    feat(PetRegisterScreen): feito importação do FormPet

--- src/screens/PetRegisterScreen/index.tsx
+++ src/screens/PetRegisterScreen/index.tsx
@@ -1,10 +1,12 @@
 import LayoutWrapper from "../../components/LayoutWrapper";
 import HeaderForm from "../../components/HeaderForm";
+import FormPet from "../../components/FormPet";
 
 export default function PetRegisterScreen() {
   return (
     <LayoutWrapper isDogPawBottomTop={true}>
       <HeaderForm initialStep={2} />
+      <FormPet />
     </LayoutWrapper>
   );
 }
