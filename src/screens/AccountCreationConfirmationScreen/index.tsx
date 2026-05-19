89777a15067f480044cd2d5aff9ffd47db24ea54
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 10:46:55 -0300

    feat(AccountCreationConfirmationScreen): criado página base AccountCreationConfirmationScreen

---
+++ src/screens/AccountCreationConfirmationScreen/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function AccountCreationConfirmationScreen() {
+  return (
+    <LayoutWrapper>
+        <Text>AccountCreationConfirmationScreen</Text>
+    </LayoutWrapper>
+  );
+}
