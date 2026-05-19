71bf307f4d15ce780816bd7af1c09d82b4b82af1
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 10:55:38 -0300

    feat(AccountCreationConfirmationScreen): adicionado ContainerTitleSubTitle

--- src/screens/AccountCreationConfirmationScreen/index.tsx
+++ src/screens/AccountCreationConfirmationScreen/index.tsx
@@ -1,10 +1,22 @@
-import { Text } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
+import ContainerTitleSubTitle from "../../components/ContainerTitleSubTitle";
+import { View } from "react-native";
 
 export default function AccountCreationConfirmationScreen() {
   return (
-    <LayoutWrapper>
-        <Text>AccountCreationConfirmationScreen</Text>
+    <LayoutWrapper isDogPawBottomTop={true}>
+      <View
+        style={{
+          flex: 1,
+          gap: 30,
+          paddingTop: 30,
+        }}
+      >
+        <ContainerTitleSubTitle
+          textTitle="Tudo certo!"
+          textSubTitle="Confira se os dados estão certos:"
+        />
+      </View>
     </LayoutWrapper>
   );
 }
