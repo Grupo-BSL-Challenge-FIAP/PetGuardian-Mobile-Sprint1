6c151add3eb82e721a637d1774ef60788bfb1ae9
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 01:47:41 -0300

    refactor(ResponsibleRegisterScreen): adicionado HeaderForm

--- src/screens/ResponsibleRegisterScreen/index.tsx
+++ src/screens/ResponsibleRegisterScreen/index.tsx
@@ -3,28 +3,12 @@ import LayoutWrapper from "../../components/LayoutWrapper";
 import ButtonBack from "../../components/ButtonBack";
 import { COLORS, FONTS } from "../../styles/styles";
 import FormResponsible from "../../components/FormResponsible";
+import HeaderForm from "../../components/HeaderForm";
 
 export default function ResponsibleRegisterScreen() {
   return (
     <LayoutWrapper isDogPawBottomTop={true}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <ButtonBack sizeIcon={35} colorIcon="black" textTrue={true} />
-        <Text
-          style={{
-            fontSize: 20,
-            fontFamily: FONTS.poppins[400],
-            color: COLORS.orange[900]
-          }}
-        >
-          Passo 1 de 3
-        </Text>
-      </View>
+      <HeaderForm initialStep={1} />
       <FormResponsible />
     </LayoutWrapper>
   );
