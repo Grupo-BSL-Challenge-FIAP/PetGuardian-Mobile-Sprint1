c60fe40ce959192f3baf79a7ca99e5f6e1dec87c
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 00:38:59 -0300

    feat(DogPaws): criado imagem das patas de fundo

---
+++ src/components/DogPaws/index.tsx
@@ -0,0 +1,26 @@
+import { Image, View } from "react-native";
+
+export default function DogPaws() {
+  return (
+    <View
+      pointerEvents="none"
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}
+    >
+      <Image
+        source={require("../../assets/dogPaws.png")}
+        style={{
+          width: "100%",
+          height: "100%",
+          resizeMode: "cover",
+          opacity: 0.75,
+        }}
+      />
+    </View>
+  );
+}
