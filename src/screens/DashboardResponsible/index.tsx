49cb8e81cbeac7dfc37dfbc49ce75117db3e5830
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 19:56:54 -0300

    feat(DashboardResponsible): criado página base DashboardResponsible

---
+++ src/screens/DashboardResponsible/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function DashboardResponsible() {
+  return (
+    <LayoutWrapper>
+      <Text>Dashboard do Responsável</Text>
+    </LayoutWrapper>
+  );
+}
