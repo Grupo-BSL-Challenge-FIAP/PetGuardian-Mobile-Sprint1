5e54517eec24267a9b9c3486399691a0f83c09be
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-19 20:37:53 -0300

    feat(AuthenticationCodeScreen): criado página base AuthenticationCodeScreen

---
+++ src/screens/AuthenticationCodeScreen/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function AuthenticationCodeScreen() {
+    return (
+        <LayoutWrapper isDogPawBottomTop={true}>
+            <Text></Text>
+        </LayoutWrapper>
+    )
+}
