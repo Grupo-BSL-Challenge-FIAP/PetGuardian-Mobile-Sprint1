8ef71b18ebf5f6e7971fbf2a871cc95c27924b3f
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 18:11:27 -0300

    feat(HeaderDashboardResponsible): criado componente HeaderDashboardResponsible

---
+++ src/components/HeaderDashboardResponsible/index.tsx
@@ -0,0 +1,79 @@
+import { Image, Text, View } from "react-native";
+import { COLORS, FONTS } from "../../styles/styles";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ResponsibleType } from "../../types/ResponsibleType";
+
+export default function HeaderDashboardResponsible() {
+  const [responsible, setResponsible] = useState<ResponsibleType>({
+    name: "",
+    email: "",
+    birthDate: "",
+    cpf: "",
+    phone: "",
+    address: "",
+  });
+
+  useEffect(() => {
+    const loadedStorage = async () => {
+      const responsibleData = await AsyncStorage.getItem(
+        "@petguardian:responsibleData",
+      );
+
+      if (responsibleData) {
+        setResponsible(JSON.parse(responsibleData));
+      }
+    };
+    loadedStorage();
+  }, []);
+
+  return (
+    <View
+      style={{
+        position: "absolute",
+        justifyContent: "center",
+        width: "100%",
+        backgroundColor: COLORS.orange[900],
+        paddingVertical: 40,
+        paddingHorizontal: 20,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10
+      }}
+    >
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 15
+      }}>
+        <Image
+          source={require("../../assets/dogPaws.png")}
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 100,
+          }}
+        />
+        <View>
+          <Text
+            style={{
+              fontFamily: FONTS.inter[700],
+              color: COLORS.white[100],
+              fontSize: 20
+            }}
+          >
+            {`Olá, ${responsible.name}!`}
+          </Text>
+          <Text
+            style={{
+              fontFamily: FONTS.inter[500],
+              color: COLORS.white[100],
+              fontSize: 15
+            }}
+          >
+            {responsible.email}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
