0f35ad9743679fabaef1669e3dce246c29343f00
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 01:22:43 -0300

    feat(ContainerButtonType): criado interface ContainerButtonType

---
+++ src/types/ContainerButtonType.ts
@@ -0,0 +1,8 @@
+export interface ContainerButtonType {
+    children: React.ReactNode;
+    marginHorizontal?: number;
+    marginTop?: number;
+    marginBottom?: number;
+    marginVertical?: number;
+    gap?: number;
+}
