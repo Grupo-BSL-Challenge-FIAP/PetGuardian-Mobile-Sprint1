3a24ec1a0bebaa04d75c8f746f0af0d60d372980
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 18:08:20 -0300

    feat(DashboardResponsibleScreen): adicionado prop isHeaderDashboardResponsible do LayoutWrapper

--- src/screens/DashboardResponsibleScreen/index.tsx
+++ src/screens/DashboardResponsibleScreen/index.tsx
@@ -1,10 +1,11 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
+import HeaderDashboardResponsible from "../../components/HeaderDashboardResponsible";
 
 export default function DashboardResponsibleScreen() {
   return (
-    <LayoutWrapper>
-      <Text>Dashboard do Responsável</Text>
+    <LayoutWrapper paddingHorizontal={-1} marginTop={90} isHeaderDashboardResponsible={true}>
+        <Text>Dashboard do Responsável</Text>
     </LayoutWrapper>
   );
 }
