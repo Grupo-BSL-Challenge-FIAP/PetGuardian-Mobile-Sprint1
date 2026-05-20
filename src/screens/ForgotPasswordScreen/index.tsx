bbe718e8a79fb833025383c88a43d79d6c656ae0
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 02:44:12 -0300

    feat(ForgotPasswordScreen): criado página base do ForgotPasswordScreen

---
+++ src/screens/ForgotPasswordScreen/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function ForgotPasswordScreen() {
+    return (
+        <LayoutWrapper isDogPawBottomTop={true}>
+            <View></View>
+        </LayoutWrapper>
+    )
+}
