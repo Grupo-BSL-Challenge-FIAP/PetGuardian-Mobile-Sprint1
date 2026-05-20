f28a78eaac64b52d0d9946b29bd2c04624e3e664
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 01:50:18 -0300

    feat(LoginScreen): adicionado logo do petGuardian

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
+import ContainerImage from "../../components/ContainerImage";
 
 export default function LoginScreen() {
   return (
     <LayoutWrapper isDogPawBottomTop={true}>
-      <View></View>
+      <ContainerImage imagePath={require("../../assets/petGuardianLogo.png")} marginTop={73} />
     </LayoutWrapper>
   );
 }
