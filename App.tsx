f401e17dd5e6358497816481a6d2a8f8065d6e15
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 17:30:19 -0300

    feat(App): feito importação do fonts/inter

--- App.tsx
+++ App.tsx
@@ -9,12 +9,24 @@ import {
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
 
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     "Poppins-Regular": Poppins_400Regular,
     "Poppins-Medium": Poppins_500Medium,
     "Poppins-SemiBold": Poppins_600SemiBold,
     "Poppins-Bold": Poppins_700Bold,
+
+    "Inter-Regular": Inter_400Regular,
+    "Inter-Medium": Inter_500Medium,
+    "Inter-SemiBold": Inter_600SemiBold,
+    "Inter-Bold": Inter_700Bold,
   });
 
   if (!fontsLoaded) {
