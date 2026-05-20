8829a0535bded7f5d860168209a97acd938df6ab
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 01:59:42 -0300

    feat(LoginScreen): adicionado ContainerTitleSubTitle

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,11 +1,19 @@
 import { Text, View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
 import ContainerImage from "../../components/ContainerImage";
+import ContainerTitleSubTitle from "../../components/ContainerTitleSubTitle";
+import { FONTS } from "../../styles/styles";
 
 export default function LoginScreen() {
   return (
     <LayoutWrapper isDogPawBottomTop={true}>
       <ContainerImage imagePath={require("../../assets/petGuardianLogo.png")} marginTop={73} />
+
+      <ContainerTitleSubTitle 
+        textTitle={`Que bom ver\nvocê novamente!`}
+        textSubTitle="Insira seus dados para continuar."
+        fontFamilySubTitle={FONTS.poppins[700]}
+      />
     </LayoutWrapper>
   );
 }
