5cfcc9af955f04390b8c7333cc31a3ceb8ea8431
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 00:42:15 -0300

    feat(ButtonType): criado interface ButtonType

---
+++ src/types/ButtonType.ts
@@ -0,0 +1,16 @@
+export interface ButtonType {
+  onPress: () => void;
+  paddingVertical?: number;
+  alignItems?: "center" | "flex-start" | "flex-end";
+  borderRadius?: number;
+  backgroundColor?: string;
+  fontFamily?: string;
+  fontSize?: number;
+  colorText?: string;
+  borderColor?: string;
+  borderWidth?: number;
+  children: React.ReactNode;
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
+  gapTextIcon?: number;
+}
