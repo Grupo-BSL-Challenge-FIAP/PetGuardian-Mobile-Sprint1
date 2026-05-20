b95973cc2baab1b30b485c80c22770860b3ee161
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 03:57:48 -0300

    feat(ForgotPasswordScreen): adicionado ContainerTitleSubTitle

--- src/screens/ForgotPasswordScreen/index.tsx
+++ src/screens/ForgotPasswordScreen/index.tsx
@@ -1,10 +1,22 @@
 import { View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
+import ContainerImage from "../../components/ContainerImage";
+import ContainerTitleSubTitle from "../../components/ContainerTitleSubTitle";
 
 export default function ForgotPasswordScreen() {
-    return (
-        <LayoutWrapper isDogPawBottomTop={true}>
-            <View></View>
-        </LayoutWrapper>
-    )
+  return (
+    <LayoutWrapper
+      isDogPawBottomTop={true}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <ContainerImage imagePath={require("../../assets/forgotPassword.png")} />
+
+      <ContainerTitleSubTitle 
+        textTitle="Esqueceu sua senha?"
+        textSubTitle={`Informe seu e-mail para\nredefinir sua senha`}
+        marginTop={20}
+      />
+    </LayoutWrapper>
+  );
 }
