03874b3a9c37afc54e54d635398ba62292710c83
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 18:40:27 -0300

    feat(TitleOrange): adicionado a prop fontFamily

--- src/components/TitleOrange/index.tsx
+++ src/components/TitleOrange/index.tsx
@@ -2,12 +2,12 @@ import { Text } from "react-native";
 import { COLORS, FONTS } from "../../styles/styles";
 import { TitleOrangeType } from "../../types/TitleOrangeType";
 
-export default function TitleOrange({ title, fontSize, textAlign }: TitleOrangeType) {
+export default function TitleOrange({ title, fontSize, textAlign, fontFamily }: TitleOrangeType) {
   return (
     <Text
       style={{
         color: COLORS.orange[900],
-        fontFamily: FONTS.poppins[700],
+        fontFamily: fontFamily || FONTS.poppins[700],
         fontSize: fontSize || 32,
         textAlign: textAlign || "left",
       }}
