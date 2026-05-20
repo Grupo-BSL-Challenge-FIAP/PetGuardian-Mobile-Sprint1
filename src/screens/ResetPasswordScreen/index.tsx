5b669454711a51b24ff16fb1fbeef32aeeb2917b
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 04:46:50 -0300

    feat(ResetPasswordScreen): criado página do ResetPasswordScreen faltando somente as validações

--- src/screens/ResetPasswordScreen/index.tsx
+++ src/screens/ResetPasswordScreen/index.tsx
@@ -1,10 +1,46 @@
 import { View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
+import ContainerImage from "../../components/ContainerImage";
+import ContainerTitleSubTitle from "../../components/ContainerTitleSubTitle";
+import { BACKGROUND, COLORS, FONTS } from "../../styles/styles";
+import ContainerForm from "../../components/ContainerForm";
+import InputForm from "../../components/InputForm";
+import ButtonFormLink from "../../components/ButtonFormLink";
+import { Feather } from "@expo/vector-icons";
 
 export default function ResetPasswordScreen() {
-    return (
-        <LayoutWrapper>
-            <View></View>
-        </LayoutWrapper>
-    )
+  return (
+    <LayoutWrapper isDogPawBottomTop={true} justifyContent="center">
+      <ContainerImage imagePath={require("../../assets/resetPassword.png")} />
+
+      <ContainerTitleSubTitle
+        textTitle="Digite sua nova senha"
+        textSubTitle={`Dica: guarde sua nova senha\ncom cuidado.`}
+        fontFamilySubTitle={FONTS.poppins[600]}
+        marginTop={20}
+        marginBottom={20}
+      />
+
+      <ContainerForm>
+        <InputForm label="Nova senha:" placeholder="Crie uma nova senha" />
+        <InputForm
+          label="Confirme sua senha:"
+          placeholder="Repita a nova senha"
+        />
+
+        <ButtonFormLink
+          onPress={() => {}}
+          backgroundColor={COLORS.orange[900]}
+          colorText={COLORS.white[100]}
+          borderWidth={2}
+          paddingVertical={8}
+          iconRight={
+            <Feather name="refresh-ccw" size={32} color={COLORS.white[100]} />
+          }
+        >
+          Redefinir senha
+        </ButtonFormLink>
+      </ContainerForm>
+    </LayoutWrapper>
+  );
 }
