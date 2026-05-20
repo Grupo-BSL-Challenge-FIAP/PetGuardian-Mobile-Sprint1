5d0085e26d4a78ba60242cd5e33141c525d94fd0
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-20 01:46:28 -0300

    feat(LoginScreen): adicionado LayoutWrapper

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <LayoutWrapper isDogPawBottomTop={true}>
+      <View></View>
+    </LayoutWrapper>
   );
 }
