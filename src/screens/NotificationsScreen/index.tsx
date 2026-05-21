633a113fe92d35906163cce322e209c9513f47bf
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 15:44:39 -0300

    feat(NotificationsScreen): criado página base do NotificationsScreen

---
+++ src/screens/NotificationsScreen/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function NotificationsScreen() {
+    return (
+        <LayoutWrapper>
+            <View></View>
+        </LayoutWrapper>
+    )
+}
