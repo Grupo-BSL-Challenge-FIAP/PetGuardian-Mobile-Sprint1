1aebddc3901e67a304885711cb5ba79a74b6b356
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 20:02:40 -0300

    feat(AppNavigator): criado rotas para PetsScreen, NotificationsScreen, ProfileScreen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -10,6 +10,7 @@ import ForgotPasswordScreen from "../screens/ForgotPasswordScreen";
 import ResetPasswordScreen from "../screens/ResetPasswordScreen";
 import PetsScreen from "../screens/PetsScreen";
 import NotificationsScreen from "../screens/NotificationsScreen";
+import TabNavigator from "./TabNavigator";
 
 export type RootStackParamList = {
   HomeLoginCadastroScreen: undefined;
@@ -23,102 +24,117 @@ export type RootStackParamList = {
   ResetPasswordScreen: undefined;
   PetsScreen: undefined;
   NotificationsScreen: undefined;
+  ProfileResponsibleScreen: undefined;
+  TabsDashboardResponsible: undefined;
+  TabsPet: undefined;
+  TabsNotification: undefined;
+  TabsProfileScreen: undefined;
+  Início: undefined;
+  Pets: undefined;
+  Notificações: undefined;
+  Perfil: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
-      <Stack.Navigator
-        initialRouteName='HomeLoginCadastroScreen'
-      >
-        <Stack.Screen 
-            name="HomeLoginCadastroScreen"
-            component={HomeLoginCadastroScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+    <Stack.Navigator initialRouteName="HomeLoginCadastroScreen">
+      <Stack.Screen
+        name="HomeLoginCadastroScreen"
+        component={HomeLoginCadastroScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="LoginScreen"
-            component={LoginScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="ResponsibleRegisterScreen"
-            component={ResponsibleRegisterScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="ResponsibleRegisterScreen"
+        component={ResponsibleRegisterScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="PetRegisterScreen"
-            component={PetRegisterScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="PetRegisterScreen"
+        component={PetRegisterScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="AccountCreationConfirmationScreen"
-            component={AccountCreationConfirmationScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="AccountCreationConfirmationScreen"
+        component={AccountCreationConfirmationScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="DashboardResponsibleScreen"
-            component={DashboardResponsibleScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="TabsDashboardResponsible"
+        component={TabNavigator}
+        options={{
+          headerShown: false,
+        }}
+      />
 
-        <Stack.Screen 
-            name="AuthenticationCodeScreen"
-            component={AuthenticationCodeScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="AuthenticationCodeScreen"
+        component={AuthenticationCodeScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="ForgotPasswordScreen"
-            component={ForgotPasswordScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="ForgotPasswordScreen"
+        component={ForgotPasswordScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="ResetPasswordScreen"
-            component={ResetPasswordScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-            />
+      <Stack.Screen
+        name="ResetPasswordScreen"
+        component={ResetPasswordScreen}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="PetsScreen"
-            component={PetsScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
+      <Stack.Screen
+        name="TabsPet"
+        component={TabNavigator}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
 
-        <Stack.Screen 
-            name="NotificationsScreen"
-            component={NotificationsScreen}
-            options={({ navigation }) => ({
-              headerShown: false
-            })}    
-        />
-      </Stack.Navigator>
+      <Stack.Screen
+        name="TabsNotification"
+        component={TabNavigator}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
+
+      <Stack.Screen
+        name="TabsProfileScreen"
+        component={TabNavigator}
+        options={({ navigation }) => ({
+          headerShown: false,
+        })}
+      />
+    </Stack.Navigator>
   );
 }
