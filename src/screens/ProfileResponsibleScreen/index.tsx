8f6d531995457f6e1564e656c1289959b6ff8c11
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 19:39:35 -0300

    feat(ProfileResponsibleScreen): criado página base ProfileResponsibleScreen

---
+++ src/screens/ProfileResponsibleScreen/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function ProfileResponsibleScreen() {
+    return (
+        <LayoutWrapper>
+            <View></View>
+        </LayoutWrapper>
+    )
+}
