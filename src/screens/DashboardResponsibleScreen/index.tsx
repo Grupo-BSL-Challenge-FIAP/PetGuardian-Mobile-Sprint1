065745815b70cc15942e7ae2af8b5a8c1b274111
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 11:27:34 -0300

    style(DashboardResponsibleScreen): removido marginTop do LayoutWrapper

--- src/screens/DashboardResponsibleScreen/index.tsx
+++ src/screens/DashboardResponsibleScreen/index.tsx
@@ -1,12 +1,11 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
-import HeaderDashboardResponsible from "../../components/HeaderDashboardResponsible";
 import YourPets from "../../components/YourPets";
 import StatusPet from "../../components/StatusPet";
 
 export default function DashboardResponsibleScreen() {
   return (
-    <LayoutWrapper paddingHorizontal={-1} marginTop={90} isHeaderDashboardResponsible={true}>
+    <LayoutWrapper paddingHorizontal={-1} isHeaderDashboardResponsible={true}>
       <View
         style={{
           paddingHorizontal: 15,
@@ -14,8 +13,8 @@ export default function DashboardResponsibleScreen() {
           gap: 30
         }}
       >
-        <YourPets />
-        <StatusPet />
+          <YourPets />
+          <StatusPet />
       </View>
     </LayoutWrapper>
   );
