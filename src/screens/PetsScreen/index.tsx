ecb52d1d8d3e62f7fff27365f5a616b9fe7b003f
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 10:55:34 -0300

    feat(PetsScreen): criado página base do PetsScreen

---
+++ src/screens/PetsScreen/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function PetsScreen() {
+    return (
+        <LayoutWrapper>
+            <View></View>
+        </LayoutWrapper>
+    )
+}
