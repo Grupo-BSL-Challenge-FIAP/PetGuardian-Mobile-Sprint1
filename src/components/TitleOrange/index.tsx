3868332d99a8a2e96d0e3452f5938a3b7b2f18d1
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 02:57:52 -0300

    feat(TitleOrange): adicionado as props marginLeft e color

--- src/components/TitleOrange/index.tsx
+++ src/components/TitleOrange/index.tsx
@@ -2,14 +2,15 @@ import { Text } from "react-native";
 import { COLORS, FONTS } from "../../styles/styles";
 import { TitleOrangeType } from "../../types/TitleOrangeType";
 
-export default function TitleOrange({ title, fontSize, textAlign, fontFamily }: TitleOrangeType) {
+export default function TitleOrange({ title, fontSize, textAlign, fontFamily, marginLeft, color }: TitleOrangeType) {
   return (
     <Text
       style={{
-        color: COLORS.orange[900],
+        color: color || COLORS.orange[900],
         fontFamily: fontFamily || FONTS.poppins[700],
         fontSize: fontSize || 32,
         textAlign: textAlign || "left",
+        marginLeft: marginLeft || 0
       }}
     >
       {title}
