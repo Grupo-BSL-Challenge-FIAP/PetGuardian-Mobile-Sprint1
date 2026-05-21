2109fcbe4ac577919fcc912d8f806ae3a81dcb58
moisesBarsoti <moises.barsot2@gmail.com>
2026-05-21 15:40:58 -0300

    feat(DashboardResponsibleScreen): feito importação do ContainerInsights

--- src/screens/DashboardResponsibleScreen/index.tsx
+++ src/screens/DashboardResponsibleScreen/index.tsx
@@ -2,6 +2,7 @@ import { ScrollView, Text, View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
 import YourPets from "../../components/YourPets";
 import StatusPet from "../../components/StatusPet";
+import ContainerInsights from "../../components/ContainerInsights";
 
 export default function DashboardResponsibleScreen() {
   return (
@@ -15,6 +16,7 @@ export default function DashboardResponsibleScreen() {
       >
           <YourPets />
           <StatusPet />
+          <ContainerInsights />
       </View>
     </LayoutWrapper>
   );
