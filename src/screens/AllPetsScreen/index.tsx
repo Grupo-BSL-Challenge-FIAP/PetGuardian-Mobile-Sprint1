cee2ab75eedefad35dbe21b8d13d0384e66134d0
moisesBarsoti <moises.barsot2@gmail.com>
2026-08-17 15:27:01 -0300

    feat(AllPetsScreen): criado base AllPetsScreen

---
+++ src/screens/AllPetsScreen/index.tsx
@@ -0,0 +1,9 @@
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function AllPetsScreen() {
+    return (
+        <LayoutWrapper>
+            <></>
+        </LayoutWrapper>
+    )
+}
