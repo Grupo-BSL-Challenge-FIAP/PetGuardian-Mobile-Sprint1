5c819a7d65f4934c32c223bd34a73f0086c720eb
moisesBarsoti <moises.barsot2@gmail.com>
2026-08-23 17:00:35 -0300

    feat(TabNavigator): adicionado rota AllPetsScreen

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,13 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DashboardResponsibleScreen from "../screens/DashboardResponsibleScreen";
 import { RootStackParamList } from "./AppNavigator";
-import PetsScreen from "../screens/PetsScreen";
-import NotificationsScreen from "../screens/NotificationsScreen";
 import { COLORS, FONTS } from "../styles/styles";
-import ProfileResponsibleScreen from "../screens/ProfileResponsibleScreen";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { View } from "react-native";
 import PageUnderDevelopmentScreen from "../screens/PageUnderDevelopmentScreen";
+import AllPetsScreen from "../screens/AllPetsScreen";
 
 const Tab = createBottomTabNavigator<RootStackParamList>();
 
@@ -74,7 +72,7 @@ export default function TabNavigator() {
 
       <Tab.Screen
         name="Pets"
-        component={PageUnderDevelopmentScreen}
+        component={AllPetsScreen}
         options={{
           tabBarIcon: ({ focused }) => (
             <View
