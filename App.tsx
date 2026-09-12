4678cb5e99359d0a4ce7be490b6767f3a14946d9
moisesBarsoti <moises.barsoti@taak.com.br>
2026-09-11 21:56:53 -0300

    feat(App): importado QueryClientProvider com queryClient

--- App.tsx
+++ App.tsx
@@ -15,6 +15,8 @@ import {
   Inter_600SemiBold,
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./src/api/queryClient";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -34,8 +36,10 @@ export default function App() {
   }
 
   return (
+  <QueryClientProvider client={queryClient}>
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
+  </QueryClientProvider>
   );
 }
