f7e94c0f5ce638999bc35e681568b3a75576524e
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-13 17:15:05 -0300

    feat(AccountUserScreen): criado página base AccountUserScreen

---
+++ src/screens/AccountUserScreen/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import LayoutWrapper from "../../components/LayoutWrapper";
+
+export default function AccountUserScreen() {
+    return (
+        <LayoutWrapper>
+            <View></View>
+        </LayoutWrapper>
+    )
+}
