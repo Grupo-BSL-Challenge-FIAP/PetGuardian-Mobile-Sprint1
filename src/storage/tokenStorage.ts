cac3eadef231d476376c6faae849a5c2b9fb1d20
moisesBarsoti <moises.barsoti@taak.com.br>
2026-09-12 22:26:42 -0300

    feat(tokenStorage): criado tokenStorage

---
+++ src/storage/tokenStorage.ts
@@ -0,0 +1,24 @@
+import * as SecureStore from "expo-secure-store";
+
+const TOKEN_KEY = "@vitalia:token";
+
+export const tokenStorage = {
+  save: async (token: string): Promise<void> => {
+    await SecureStore.setItemAsync(
+      TOKEN_KEY,
+      token,
+    );
+  },
+
+  get: async (): Promise<string | null> => {
+    return SecureStore.getItemAsync(
+      TOKEN_KEY,
+    );
+  },
+
+  remove: async (): Promise<void> => {
+    await SecureStore.deleteItemAsync(
+      TOKEN_KEY,
+    );
+  },
+};
