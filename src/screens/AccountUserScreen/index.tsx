cd036a7275773dac517f5416a7c53c443493f972
moisesBarsoti <moises.barsot2@gmail.com>
2026-09-13 18:53:34 -0300

    feat(AccountUserScreen): implementado Header e CardProfileUser

--- src/screens/AccountUserScreen/index.tsx
+++ src/screens/AccountUserScreen/index.tsx
@@ -1,10 +1,22 @@
 import { View } from "react-native";
 import LayoutWrapper from "../../components/LayoutWrapper";
+import Header from "../../components/Header";
+import CardProfileUser from "../../components/CardProfileUser";
 
 export default function AccountUserScreen() {
-    return (
-        <LayoutWrapper>
-            <View></View>
-        </LayoutWrapper>
-    )
+  return (
+    <LayoutWrapper paddingHorizontal={-1} paddingBottom={80}>
+      <Header isBackBottom={false} textHeader="Perfil" />
+
+      <View
+        style={{
+          paddingHorizontal: 12,
+          marginTop: 20,
+          gap: 30,
+        }}
+      >
+        <CardProfileUser />
+      </View>
+    </LayoutWrapper>
+  );
 }
